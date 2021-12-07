dd4a78b4b441143a1fbc4579785d2b6e94633c7b
nguyenkhanh <nguyenquockhanh12a9@gmail.com>
2021-12-07 21:43:05 +0700

    "get and set products"

--- app/src/main/java/vn/stu/edu/doancn/model/Products.java
+++ app/src/main/java/vn/stu/edu/doancn/model/Products.java
@@ -3,4 +3,20 @@ package vn.stu.edu.doancn.model;
 public class Products {
     private String ma;
     private String gia;
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getGia() {
+        return gia;
+    }
+
+    public void setGia(String gia) {
+        this.gia = gia;
+    }
 }
