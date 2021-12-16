5845c976bf415699f492011020c6b616a958c95e
acer <nhahc0110@gmail.com>
2021-12-16 21:31:50 +0700

    them edittext

--- app/src/main/java/vn/stu/edu/doancn/admin/CreateAccountAdminActivity.java
+++ app/src/main/java/vn/stu/edu/doancn/admin/CreateAccountAdminActivity.java
@@ -3,11 +3,12 @@ package vn.stu.edu.doancn.admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import vn.stu.edu.doancn.R;
 
 public class CreateAccountAdminActivity extends AppCompatActivity {
-
+    EditText user, password, diachi, email, sdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
