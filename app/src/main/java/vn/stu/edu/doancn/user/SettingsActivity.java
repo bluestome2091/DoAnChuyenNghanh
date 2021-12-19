ef9c1322b4d9ce1467d6a47dcbdccd5bca6551fc
nguyenkhanh <nguyenquockhanh12a9@gmail.com>
2021-12-19 12:57:34 +0700

    Sửa xong lỗi không truy cập được profile sau khi sửa xong giao diện

--- app/src/main/java/vn/stu/edu/doancn/user/SettingsActivity.java
+++ app/src/main/java/vn/stu/edu/doancn/user/SettingsActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -39,9 +41,9 @@ import vn.stu.edu.doancn.R;
 public class SettingsActivity extends AppCompatActivity {
 
     private CircleImageView settings_profile_image;
-    private EditText settings_name, settings_phone, settings_address;
+    private TextInputEditText settings_name, settings_phone, settings_address, settings_password;
     private TextView profile_image_change_btn, close_settings_btn, update_settings_btn;
-
+    private Button btnEditSt;
     private Uri imageUri;
     private String myUrl = "";
     private StorageReference storageProfileReference;
@@ -55,11 +57,11 @@ public class SettingsActivity extends AppCompatActivity {
 
         storageProfileReference = FirebaseStorage.getInstance().getReference().child("Profile pictures");
         addControls();
-        addEvents();
-        userInfoDisplay(settings_profile_image, settings_name, settings_phone, settings_address);
+//        addEvents();
+        userInfoDisplay();
     }
 
-    private void userInfoDisplay(CircleImageView settings_profile_image, EditText settings_name, EditText settings_phone, EditText settings_address)            {
+    private void userInfoDisplay()            {
         DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(Prevalent.currentOnlineUser.getUsers());
         UsersRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -70,11 +72,12 @@ public class SettingsActivity extends AppCompatActivity {
                         String name = snapshot.child("name").getValue().toString();
                         String phone = snapshot.child("phone").getValue().toString();
                         String address = snapshot.child("address").getValue().toString();
-
+                        String password = snapshot.child("Password").getValue().toString();
                         Picasso.get().load(image).into(settings_profile_image);
                         settings_name.setText(name);
                         settings_phone.setText(phone);
                         settings_address.setText(address);
+                        settings_password.setText(password);
                     }
                 }
             }
@@ -206,11 +209,13 @@ public class SettingsActivity extends AppCompatActivity {
 
     private void addControls() {
         settings_profile_image = findViewById(R.id.settings_profile_image);
-        settings_name = findViewById(R.id.settings_name);
-        settings_phone = findViewById(R.id.settings_phone);
-        settings_address = findViewById(R.id.settings_address);
+        settings_name = findViewById(R.id.txtName_st);
+        settings_phone = findViewById(R.id.txtPhone_st);
+        settings_address = findViewById(R.id.txtAddress_st);
+        settings_password = findViewById(R.id.txtPassword_st);
         profile_image_change_btn = findViewById(R.id.profile_image_change_btn);
         close_settings_btn = findViewById(R.id.close_settings_btn);
         update_settings_btn = findViewById(R.id.update_settings_btn);
+        btnEditSt = findViewById(R.id.btnEditSt);
     }
 }
