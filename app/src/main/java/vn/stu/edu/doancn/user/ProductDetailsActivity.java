96e566b9e284278d05a819faa4eea2e774715bcb
acer <nhahc0110@gmail.com>
2021-12-20 21:31:19 +0700

    "xóa number_btn"

--- app/src/main/java/vn/stu/edu/doancn/user/ProductDetailsActivity.java
+++ app/src/main/java/vn/stu/edu/doancn/user/ProductDetailsActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+//import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import vn.stu.edu.doancn.R;
@@ -15,7 +15,7 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
     private FloatingActionButton add_product_to_cart;
     private ImageView product_image_details;
-    private ElegantNumberButton number_btn;
+//    private ElegantNumberButton number_btn;
     private TextView txtProductname_details, txtDescription_details, txtPrice_details;
 
     @Override
@@ -33,7 +33,7 @@ public class ProductDetailsActivity extends AppCompatActivity {
     private void addControls() {
         add_product_to_cart=findViewById(R.id.add_product_to_cart);
         product_image_details=findViewById(R.id.product_image_details);
-        number_btn=findViewById(R.id.number_btn);
+//        number_btn=findViewById(R.id.number_btn);
         txtProductname_details=findViewById(R.id.txtProductname_details);
         txtPrice_details=findViewById(R.id.txtPrice_details);
         txtDescription_details=findViewById(R.id.txtDescription_details);
